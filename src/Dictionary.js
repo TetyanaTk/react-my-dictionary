b96aa32dde90d902cbde269f27a4e81ba3424876
TetyanaTk <106308840+TetyanaTk@users.noreply.github.com>
2022-07-24 23:57:13 +0300

    Changed the key

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary(props) {
     axios.get(apiUrl).then(handleDictionResponse);
 
     let pexelsApiKey =
-      "563492ad6f91700001000001fdd29f0808df42bd90c33f42e128fa89";
+      "563492ad6f91700001000001e65d28f3eaae4ce892f284ff349597e2";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
